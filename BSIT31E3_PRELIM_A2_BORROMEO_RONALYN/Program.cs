dcc7307e1663c96aafc55d79cc8e5dfbb1919118
BORROMEO, RONALYN, MIRANDA <1518-22@itmlyceumalabang.onmicrosoft.com>
2026-06-20 08:51:36 +0800

    Commit 3: Add calculator operations

--- BSIT31E3_PRELIM_A2_BORROMEO_RONALYN/Program.cs
+++ BSIT31E3_PRELIM_A2_BORROMEO_RONALYN/Program.cs
@@ -4,5 +4,24 @@
     string input = Console.ReadLine();
     if (input.ToLower() == "exit") break;
 
-    // TODO: Get second number, get operator, calculate, and display.
+    while (true)
+    {
+        Console.Write("Enter first number (or 'exit' to quit): ");
+        string input = Console.ReadLine();
+        if (input.ToLower() == "exit") break;
+
+        double num1 = Convert.ToDouble(input);
+        Console.Write("Enter second number: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Enter operator: ");
+        string op = Console.ReadLine();
+
+        double result = 0;
+        if (op == "+") result = num1 + num2;
+        else if (op == "-") result = num1 - num2;
+        else if (op == "*") result = num1 * num2;
+        else if (op == "/") result = num1 / num2;
+
+        Console.WriteLine("Result: " + result);
+    }
 }
